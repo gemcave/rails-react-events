d4abe8161247638286d95d5d84d654f4314249c6
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-10-23 23:56:49 +0600

    webpacker and event controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	Rails.application.routes.draw do
+		namespace :api do
+			resources :events, only: %i[index show create destroy update]
+		end
+	end
 end
